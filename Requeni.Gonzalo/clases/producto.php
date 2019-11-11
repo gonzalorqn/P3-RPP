60ea6b65a2364efd377106cc1d88b0b65b492967
gonzalorqn <36246342+gonzalorqn@users.noreply.github.com>
2019-11-11 11:01:23 -0300

    Add files via upload

--- Requeni.Gonzalo/clases/producto.php
+++ Requeni.Gonzalo/clases/producto.php
@@ -4,5 +4,25 @@ require "IParte2.php";
 require_once 'AccesoDatos.php';
 class Producto implements IParte2
 {
+    public $codigoBarra;
+    public $descripcion;
+    public $precio;
+    public $pathFoto;
+
+    public function __construct($codigoBarra=NULL, $descripcion=NULL, $precio=NULL,$pathFoto=NULL)
+	{
+        if($codigoBarra!=NULL)
+            $this->codigoBarra = $codigoBarra;
+        if($descripcion!=NULL)
+            $this->descripcion = $descripcion;
+        if($precio!=NULL)
+            $this->precio = $precio;
+        if($pathFoto!=NULL)
+            $this->pathFoto = $pathFoto;
+    }
     
+    public function ToJSON()
+    {
+        return '{"codigoBarra":"'.$this->codigoBarra.'","descripcion":"'.$this->descripcion.'","precio":"'.$this->precio.'","pathFoto":"'.$this->pathFoto.'"}';
+    }
 }
